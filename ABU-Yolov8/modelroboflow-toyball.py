db089f8d653e6e53a63483e665311b409dbd5f2f
Theeramet Chuaipayung <threeamet2@gmail.com>
2024-09-27 16:50:16 +0700

    Robocon 2024

--- ABU-Yolov8/modelroboflow-toyball.py
+++ ABU-Yolov8/modelroboflow-toyball.py
@@ -1,7 +1,6 @@
 import cv2
 from ultralytics import YOLO
-from ultralytics.models.yolo.detect.predict import DetectionPredictor # Updated import
-
+from ultralytics.models.yolo.detect.predict import DetectionPredictor
 skip_frames = 5
 
 model = YOLO('D:/ThedboyCoding/ABU Yolov8/Models/roboflow_toyball.pt')
@@ -9,15 +8,13 @@ model = YOLO('D:/ThedboyCoding/ABU Yolov8/Models/roboflow_toyball.pt')
 cap = cv2.VideoCapture(0)
 
 while True:
- # Read frame
  ret, frame = cap.read()
  if not ret:
    break
 
- # Process only if it's a frame to be analyzed
  if cap.get(cv2.CAP_PROP_POS_FRAMES) % skip_frames == 0:
-   result = model(frame) # Use model prediction
-   # Process and display result (modify as needed)
+   result = model(frame) 
+   # Process and display result
    cv2.imshow('Frame', frame)
    if cv2.waitKey(1) & 0xFF == ord('q'):
      break
